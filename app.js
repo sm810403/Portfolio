dc42c48a69b4c1a8bffca323aac134958503de5d
JaeIn <sm810403@hotmail.com>
2020-08-06 15:39:21 -0700

    3rd commit
    side nav bar done

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+//loading 
 window.addEventListener('load', ()=>{
     const loader = document.querySelector('.loader');
     setTimeout(()=>{
         loader.classList.add('loader-finish')}, 3100);
     clearTimeout();
 });
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click',(e)=>{
+    document.querySelector('.nav-side').classList.add('side-tog');
+})
+const closeX = document.querySelector('.closeX');
+closeX.addEventListener('click', ()=>{
+    document.querySelector('.nav-side').classList.remove('side-tog');
+})
